28e35f1289fb86647b7cb3c1933c04c3f3d8ca26
Dilip Poudel <dilippoudel578@gmail.com>
2019-03-12 21:43:31 +0200

    Redux concept is added

--- App.js
+++ App.js
@@ -1,48 +1,33 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducers from './src/reducers';
+import { Header } from './src/components/commons'
 import firebase from 'firebase';
-import { Header, Button, Spinner, CardSection } from './src/components/common';
-import LogInForm from './src/components/LogInForm';
-
-class App extends React.Component {
-    state={ loggedIn: null};
+import LoginForm from './src/components/LoginForm';
+class App extends Component {
   componentWillMount(){
-    firebase.initializeApp({
-    apiKey: "AIzaSyADQL-A1mtaT-vYipdIUCnmg2lNzo0TORc",
-    authDomain: "authentication-c35a8.firebaseapp.com",
-    databaseURL: "https://authentication-c35a8.firebaseio.com",
-    projectId: "authentication-c35a8",
-    storageBucket: "authentication-c35a8.appspot.com",
-    messagingSenderId: "1035822666848"
-    });
-    firebase.auth().onAuthStateChanged((user)=> {
-        if(user){
-            this.setState({loggedIn: true})
-        }
-        else {
-            this.setState({loggedIn: false})
-        }
-    });
+    const config = {
+      apiKey: "AIzaSyAQNAlcTnz5Ek9ERRbmy-YVEyc3fh2of9c",
+      authDomain: "manager-e4d68.firebaseapp.com",
+      databaseURL: "https://manager-e4d68.firebaseio.com",
+      projectId: "manager-e4d68",
+      storageBucket: "manager-e4d68.appspot.com",
+      messagingSenderId: "1002556804291"
+    };
+    firebase.initializeApp(config);
   }
-  renderContent = () => {
-      switch (this.state.loggedIn) {
-          case true :
-          return <CardSection><Button onPress = {()=> firebase.auth().signOut()}>Log Out</Button></CardSection>
-          case false:
-          return <LogInForm/>
-          default: 
-          return <Spinner size = "large"/>
-      }
-  }
-  render() {
-    return (
+  render(){
+    return(
+      <Provider store = {createStore(reducers)}>
       <View>
-      <Header headerText = "Authentication" />
-        
-        {this.renderContent()}
-        
+        <Header headerText = "LogIn Form"/>
+        <LoginForm/>
       </View>
-    );
+       
+      </Provider>
+    )
   }
 }
-export default App
+export default App;
--- src/components/LogInForm.js
+++ src/components/LogInForm.js
@@ -1,75 +1,41 @@
 import React, { Component } from 'react';
-import {Text, StyleSheet} from 'react-native';
-import firebase from 'firebase';
-import {Button, Card, CardSection, Input, Spinner} from './common'
-class LogInForm extends Component {
-    state = {email : '', password : '', error: '', loading: false}
-    onButtonPress = () => {
-        const {email, password} = this.state;
-        this.setState({error: '', loading: true});
-        firebase.auth().signInWithEmailAndPassword(email, password)
-        .then(this.onLoginSuccess)
-        .catch(()=> {
-            firebase.auth().createUserWithEmailAndPassword(email,password)
-            .then(this.onLoginSuccess)
-            .catch(this.onLoginFail);
-        });
-    }
-    onLoginFail = () => {
-    this.setState({error: 'Authentication Failed.', loading: false})
-    }
-    onLoginSuccess = () => {
-        this.setState({
-            email: '',
-            password: '',
-            loading: false,
-            error: ''
-        })
-    }
-    renderButton= ()=> {
-        if(this.state.loading){
-            return <Spinner size = "small"/>
-        }
-        return (
-        <Button onPress = {this.onButtonPress}> Log In
-        </Button>);
-
+import { Card, Input, Button, CardSection } from './commons';
+import {connect} from 'react-redux';
+import { emailChanged } from '../actions/index';
+class LoginForm extends Component {
+    onEmailChange(text){
+        this.props.emailChanged(text);
     }
     render() {
         return (
             <Card>
                 <CardSection>
-                    <Input
-                    label = "Email"
-                    placeholder = "user@gmail.com"
-                    value = {this.state.email}
-                    onChangeText = {(email) => this.setState({email})}
+                    <Input label = "Email"
+                    placeholder = "email@gmail.com"
+                    onChangeText ={this.onEmailChange.bind(this)}
+                    value = {this.props.email}
                     />
                 </CardSection>
-
                 <CardSection>
                 <Input
-                    label = "Password"
-                    placeholder = "password"
-                    secureTextEntry
-                    value = {this.state.password}
-                    onChangeText = {(password) => this.setState({password})}
-                    />
-
+                secureTextEntry
+                label = "Password"
+                placeholder= "password"
+                />
                 </CardSection>
-                <Text style = {styles.errorTextStyle}>{this.state.error}</Text>
                 <CardSection>
-                    {this.renderButton()}
+                    <Button>
+                        Login
+                    </Button>
                 </CardSection>
             </Card>
         );
     }
 }
-const styles = StyleSheet.create({
-    errorTextStyle: {
-        fontSize: 20,
-        alignSelf: 'center',
-        color: 'red'
+const mapStateToProps = state => {
+    return {
+        email: state.auth.email
     }
-})
-export default LogInForm; 
+}
+
+export default connect(mapStateToProps, { emailChanged })(LoginForm);
